330515a4dae49ef47428990c6ec45575a4999fa2
Myung Jin Song <jsong220@gmail.com>
2020-10-28 19:47:41 -0700

    Update Space.cpp

--- Space.cpp
+++ Space.cpp
@@ -1,6 +1,5 @@
 /****************************************************************************
 **Author: Myung Jin Song
-**Date: 12-5-17
 **Description: default constructors and destructor so that 
 objects can be inititated. 
 -All other functions will be pure virtual functions as derived classes
